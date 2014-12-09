c6ebac0da1d0027745983dd5a452b435e805ba4a
Mike Gaughan <michael.t.gaughan@gmail.com>
2014-12-09 01:06:41 -0700

    Added README

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,8 @@
  * Controller of the swFrontApp
  */
 angular.module('swFrontApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $resource) {
+  	$resource('/api/edges').query();
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
