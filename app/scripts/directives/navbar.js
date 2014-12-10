5a9a77f77226eae9f9bd90bca8fb7835d9309e24
Mike Gaughan <michael.t.gaughan@gmail.com>
2014-12-09 21:49:39 -0700

    nav into directive

---
+++ app/scripts/directives/navbar.js
@@ -0,0 +1,11 @@
+'use strict'
+
+angular.module('swFrontApp').directive('navbar', function(){
+
+	return {
+		restrict: 'E',
+		templateUrl: 'views/navbar.html',
+		controller: 'NavigationController'
+	}
+
+});
